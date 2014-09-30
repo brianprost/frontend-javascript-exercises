5e72cedeffd764e1d2c2ab1127388b9c23e67a43
Brian Prost <bprost94@gmail.com>
2014-09-30 22:36:48 +0000

    Having trouble with the findFirstMatch function in challenge.js

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,9 +1,10 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + ",\n\n" + message + "\n\nSincerely,\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  // this is where I'm beginning to have trouble
+  return bigString.substring(startA, endA);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
