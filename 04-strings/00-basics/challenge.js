f890b892515f03e0f218e5cb66f3c52d149cd0df
Brian Prost <bprost94@gmail.com>
2014-10-02 19:52:50 +0000

    finished the string checkpoint

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -19,8 +19,15 @@ module.exports.findLastMatch = function(text, searchString) {
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-  var toStart = text.indexOf(searchString);
+  var toStart = text.indexOf(searchString) + searchString.length;
   var toEnd = text.lastIndexOf(searchString);
   var result = text.substring(toStart, toEnd);
   return result;
 };
+
+/* Complete the substringBetweenMatches function. This function should:
+
+Take two strings as arguments. The first string is the one to search, the second is the one to search for
+Return the substring between the first match and the last match
+Not include the first match or the last match in the returned substring For example:
+findFirst("Roses are red, apples are really red.", "red") // returns ", apples are really " */
