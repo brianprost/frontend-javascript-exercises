8921e47466942010e0bfff5e1e5bd5f365ec0019
Brian Prost <bprost94@gmail.com>
2014-10-03 21:37:49 +0000

    array functions

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (num1, num2, num3, num4) {
+  var anArray = [num1, num2, num3, num4];
+  return anArray;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function (arg1) {
+  var lastIndex = arg1.length - 1;
+  var aNewArray = [arg1[0], arg1[lastIndex]];
+  return aNewArray;
+}
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,20 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+  if (array.length < 3) {
+    var smallArray = [array[1], array[0]]
+    smallArray.unshift(1);
+    return smallArray;
+  }
+  else {
+    var lastIndex = array.length - 1;
+    var secondLastIndex = array.length - 2;
+    var largeArray = [array[lastIndex], array[secondLastIndex], array[1], array[0]];
+    largeArray.unshift(1);
+    return largeArray;
+  }
+}
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function (anArray) {
+  var ThreeStringedArray = array[] // im not done with this part yet
+}
 
 module.exports.arrayQuantityPlusOne = undefined;
