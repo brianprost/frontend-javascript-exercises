707c06db71cefde1bcfe0faa81771505ba4a63ce
Brian Prost <bprost94@gmail.com>
2014-10-14 20:27:28 +0000

    removed c9 stuff

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,28 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function (theArray) {
+    var theSum = 0;
+    for (var index = 0; index < theArray.length; index++) {
+        theSum = theSum + theArray[index];
+    }
+    return theSum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function (inputString) {
+    
+    var arrayOfStrings = inputString.split(",")
+    
+    for (var a = 0; a < arrayOfStrings.length; a++) {
+        arrayOfStrings[a] = arrayOfStrings[a].toLowerCase();
+    }
+    
+    return arrayOfStrings;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function (inputStuff) {
+    var newArray = [];
+    
+    for (var i = 0; i < inputStuff.length; i++) {
+        newArray[i] = i + " is " + inputStuff[i];
+    }
+    
+    return newArray;
+};
--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,27 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function (anObject) {
+    // return Object.keys(anObject);
+    // was this supposed to be a for - in loop? i think Object.keys does the same thing...?
+    
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+    // http://stackoverflow.com/questions/675231/how-do-i-access-properties-of-a-javascript-object-if-i-dont-know-the-names
+    // http://javascriptweblog.wordpress.com/2011/01/04/exploring-javascript-for-in-loops/
+    
+    var anArray = [];
+    for (var keys in anObject) {
+        anArray.push(keys)
+    };
+    return anArray;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function (anObject) {
+    // i know..i know...this isn't right! [-__-]
+    var anArray = [];
+    for (var property in anObject) { //uhhh... I'm supposed to return all of the keys too...
+        anArray.push(property);
+    }
+    return anArray;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function (anObject) {
+    
+};
