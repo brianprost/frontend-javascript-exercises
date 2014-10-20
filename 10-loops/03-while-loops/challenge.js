d97bd68d80385fba1d0c44e10981a5d81924b2f2
Brian Prost <bprost94@gmail.com>
2014-10-20 19:31:51 +0000

    finished all of the frontend-javascript-exercises

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,18 @@
-module.exports.stream = undefined;
+module.exports.stream = function (conditionalFn, actionFn) {
+    while (conditionalFn()) {
+        actionFn();
+    }
+};
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function (anArray) {
+    var total = 0;
+    
+    var count = 0;
+    
+    while (count < anArray.length) {
+        total += anArray[count];
+        count++;
+    }
+    
+    return total;
+};
